bc9d705e7a676316441f644a1106316466ce0ff5
pepemm <106663746+pepemm@users.noreply.github.com>
2022-11-15 11:00:37 +0100

    Add files via upload

---
+++ RETO_UNIDAD_4/RETO_UNIDAD_4_PREGUNTA10/RETO_UNIDAD_4_PREGUNTA_10_ALEAT_0/RETO_UNIDAD_4_PREGUNTA_10_ALEAT_0.py
@@ -0,0 +1,1 @@
+['Existe al menos una media que difiere del resto', 'No hay evidencias en contra de la Hipótesis nula', 'El resto de respuestas son falsas', 'Contraste significativo', 'Las media son iguales']
