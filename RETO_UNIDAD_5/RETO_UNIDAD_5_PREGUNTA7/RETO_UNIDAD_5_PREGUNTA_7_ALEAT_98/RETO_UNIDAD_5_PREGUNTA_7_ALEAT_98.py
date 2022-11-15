7a0660a1b581021a0075e629f47c8d93e02e7e20
pepemm <106663746+pepemm@users.noreply.github.com>
2022-11-15 14:34:13 +0100

    Add files via upload

---
+++ RETO_UNIDAD_5/RETO_UNIDAD_5_PREGUNTA7/RETO_UNIDAD_5_PREGUNTA_7_ALEAT_98/RETO_UNIDAD_5_PREGUNTA_7_ALEAT_98.py
@@ -0,0 +1,1 @@
+['Contraste significativo', 'Las sensibilidad y la especificidad distintas', 'No hay evidencias en contra de la Hipótesis nula', 'La sensibilidad y la especificidad son iguales', 'El resto de respuestas son falsas']
