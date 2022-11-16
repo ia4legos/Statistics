d109e3098112bd4fd875c582a795432eed43a2c9
pepemm <106663746+pepemm@users.noreply.github.com>
2022-11-16 10:05:05 +0100

    Add files via upload

---
+++ RETO_UNIDAD_5/RETO_UNIDAD_5_PREGUNTA10/RETO_UNIDAD_5_PREGUNTA_10_ALEAT_159/RETO_UNIDAD_5_PREGUNTA_10_ALEAT_159.py
@@ -0,0 +1,1 @@
+['No hay evidencias en contra de la Hipótesis nula', 'Las distribuciones son iguales', 'El resto de respuestas son falsas', 'Las distribuciones son distintas', 'Contraste significativo']
