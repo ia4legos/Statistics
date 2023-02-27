6ac813a218a1b8f3f9e7e1d310723bd5d1ccd127
pepemm <106663746+pepemm@users.noreply.github.com>
2023-02-27 14:10:19 +0100

    Add files via upload

---
+++ RETO_EXPERTO_220_230_240/RETO_EXPERTO_220_230_240_PREGUNTA5/RETO_EXPERTO_220_230_240_PREGUNTA_5_ALEAT_69/RETO_EXPERTO_220_230_240_PREGUNTA_5_ALEAT_69.py
@@ -0,0 +1,1 @@
+['plt.boxplot(inl_por_sexo, showmeans=True, meanline=True);', 'plt.boxplot(edad_por_sexo, showmeans=True, meanline=True);', 'plt.boxplot(inl_por_sexo, showbox=False, showcaps=False);', 'plt.boxplot(edad_por_sexo, showbox=False, showcaps=False);']
