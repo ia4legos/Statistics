ee708b8850b6b0385f7e18ae307364b234d36db1
pepemm <106663746+pepemm@users.noreply.github.com>
2023-02-27 11:09:45 +0100

    Add files via upload

---
+++ RETO_EXPERTO_220_230_240/RETO_EXPERTO_220_230_240_PREGUNTA3/RETO_EXPERTO_220_230_240_PREGUNTA_3_ALEAT_23/RETO_EXPERTO_220_230_240_PREGUNTA_3_ALEAT_23.py
@@ -0,0 +1,1 @@
+['plt.boxplot(linfocitos, showmeans=True, meanline=True);', 'plt.boxplot(linfocitos, notch=True);', 'plt.boxplot(linfocitos);', 'plt.boxplot(linfocitos, showbox=False, showcaps=False);']
