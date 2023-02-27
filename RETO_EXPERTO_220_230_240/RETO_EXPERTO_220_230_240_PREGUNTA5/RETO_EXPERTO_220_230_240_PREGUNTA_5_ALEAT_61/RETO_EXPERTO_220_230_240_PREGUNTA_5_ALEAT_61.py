510e720dad062d832bfc67189404566e71edd23f
pepemm <106663746+pepemm@users.noreply.github.com>
2023-02-27 14:09:51 +0100

    Add files via upload

---
+++ RETO_EXPERTO_220_230_240/RETO_EXPERTO_220_230_240_PREGUNTA5/RETO_EXPERTO_220_230_240_PREGUNTA_5_ALEAT_61/RETO_EXPERTO_220_230_240_PREGUNTA_5_ALEAT_61.py
@@ -0,0 +1,1 @@
+['plt.boxplot(leucocitos_por_sexo, vert=False);', 'plt.boxplot(leucocitos_por_sexo, showbox=False, showcaps=False);', 'plt.boxplot(glucosa_por_sexo, showbox=False, showcaps=False);', 'plt.boxplot(glucosa_por_sexo, vert=False);']
