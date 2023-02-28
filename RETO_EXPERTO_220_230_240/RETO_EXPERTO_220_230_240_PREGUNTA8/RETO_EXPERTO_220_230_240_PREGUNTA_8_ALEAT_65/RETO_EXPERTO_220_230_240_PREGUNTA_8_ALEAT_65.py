d63389083b51f8d6a09de7302e7ed1ea89c9348a
pepemm <106663746+pepemm@users.noreply.github.com>
2023-02-28 12:52:24 +0100

    Add files via upload

---
+++ RETO_EXPERTO_220_230_240/RETO_EXPERTO_220_230_240_PREGUNTA8/RETO_EXPERTO_220_230_240_PREGUNTA_8_ALEAT_65/RETO_EXPERTO_220_230_240_PREGUNTA_8_ALEAT_65.py
@@ -0,0 +1,1 @@
+["sns.catplot(data=df, x='sodio', y='target', hue='sexo', jitter=False);", "sns.catplot(data=df, x='sexo', y='sodio', hue='target');", "sns.catplot(data=df, x='sexo', y='target', hue='sodio', jitter=False);", "sns.catplot(data=df, x='sexo', y='target', hue='sodio');"]
