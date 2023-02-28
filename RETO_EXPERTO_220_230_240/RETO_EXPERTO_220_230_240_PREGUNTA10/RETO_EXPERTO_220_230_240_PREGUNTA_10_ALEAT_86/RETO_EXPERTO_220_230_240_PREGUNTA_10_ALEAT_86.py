4e63da52c7c68b1e947cd141f6d3e3ad6a1d8bfe
pepemm <106663746+pepemm@users.noreply.github.com>
2023-02-28 15:30:15 +0100

    Add files via upload

---
+++ RETO_EXPERTO_220_230_240/RETO_EXPERTO_220_230_240_PREGUNTA10/RETO_EXPERTO_220_230_240_PREGUNTA_10_ALEAT_86/RETO_EXPERTO_220_230_240_PREGUNTA_10_ALEAT_86.py
@@ -0,0 +1,1 @@
+["sns.violinplot(data=df, x='target', y='potasio', hue='sexo');", "sns.violinplot(data=df, x='target', y='sodio', hue='sexo');", "sns.violinplot(data=df, x='target', y='potasio', hue='sexo', split=True);", "sns.violinplot(data=df, x='target', y='sodio', hue='sexo', split=True);"]
