a77185b44d91afc399157f2850ec23cf2f09b9b5
pepemm <106663746+pepemm@users.noreply.github.com>
2023-02-28 12:33:31 +0100

    Add files via upload

---
+++ RETO_EXPERTO_220_230_240/RETO_EXPERTO_220_230_240_PREGUNTA7/RETO_EXPERTO_220_230_240_PREGUNTA_7_ALEAT_67/RETO_EXPERTO_220_230_240_PREGUNTA_7_ALEAT_67.py
@@ -0,0 +1,1 @@
+["sns.relplot(data=df, x='target', y='sodio', hue='glucosa', style='glucosa');", "sns.relplot(data=df, x='glucosa', y='target', hue='sodio', style='sodio');", "sns.relplot(data=df, x='glucosa', y='sodio', hue='target', style='target');", "sns.relplot(data=df, x='glucosa', y='sodio', hue='target', size='target');"]
