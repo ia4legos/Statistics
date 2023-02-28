12d60943e885342625988847277128481c9df144
pepemm <106663746+pepemm@users.noreply.github.com>
2023-02-28 14:40:27 +0100

    Add files via upload

---
+++ RETO_EXPERTO_220_230_240/RETO_EXPERTO_220_230_240_PREGUNTA10/RETO_EXPERTO_220_230_240_PREGUNTA_10_ALEAT_68/RETO_EXPERTO_220_230_240_PREGUNTA_10_ALEAT_68.py
@@ -0,0 +1,1 @@
+["sns.violinplot(data=df, x='target', y='potasio', hue='sexo', split=True);", "sns.violinplot(data=df, x='target', y='leucocitos', hue='sexo');", "sns.violinplot(data=df, x='target', y='potasio', hue='sexo', split=True);", "sns.violinplot(data=df, x='target', y='leucocitos', hue='sexo');"]
