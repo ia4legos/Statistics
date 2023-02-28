98a9f5ed285aebd74f14422cb31616f7d81706f7
pepemm <106663746+pepemm@users.noreply.github.com>
2023-02-28 15:29:36 +0100

    Add files via upload

---
+++ RETO_EXPERTO_220_230_240/RETO_EXPERTO_220_230_240_PREGUNTA10/RETO_EXPERTO_220_230_240_PREGUNTA_10_ALEAT_79/RETO_EXPERTO_220_230_240_PREGUNTA_10_ALEAT_79.py
@@ -0,0 +1,1 @@
+["sns.boxplot(data=df, x='sexo', y='linfocitos', hue='target', showmeans=True);", "sns.boxplot(data=df, x='sexo', y='inl', hue='target', showmeans=True);", "sns.violinplot(data=df, x='sexo', y='linfocitos', hue='target');", "sns.violinplot(data=df, x='sexo', y='inl', hue='target');"]
