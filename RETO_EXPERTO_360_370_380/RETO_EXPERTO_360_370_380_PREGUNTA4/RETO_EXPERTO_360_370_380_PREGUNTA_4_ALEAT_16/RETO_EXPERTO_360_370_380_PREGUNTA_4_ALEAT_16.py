c2769d7147d496bc5e952ddda9b2bd77bed49e5b
pepemm <106663746+pepemm@users.noreply.github.com>
2023-05-26 12:50:59 +0200

    Add files via upload

---
+++ RETO_EXPERTO_360_370_380/RETO_EXPERTO_360_370_380_PREGUNTA4/RETO_EXPERTO_360_370_380_PREGUNTA_4_ALEAT_16/RETO_EXPERTO_360_370_380_PREGUNTA_4_ALEAT_16.py
@@ -0,0 +1,1 @@
+['Rechazamos H0, la proporción poblacional es mayor o igual a 0.46.', 'No rechazamos H0, la proporción poblacional es mayor o igual a 0.46.', 'No rechazamos H0, la proporción poblacional es mayor o igual a 0.35.', 'Rechazamos H0, la proporción poblacional es menor que 0.35.', 'No rechazamos H0, la proporción poblacional es menor que 0.35.']
