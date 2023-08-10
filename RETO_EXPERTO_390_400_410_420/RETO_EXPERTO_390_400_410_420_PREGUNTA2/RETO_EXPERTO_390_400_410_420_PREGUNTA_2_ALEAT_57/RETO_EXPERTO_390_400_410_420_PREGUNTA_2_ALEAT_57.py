99866f66c9fc73831df0c190f7dd7176155a9dff
pepemm <106663746+pepemm@users.noreply.github.com>
2023-08-10 14:02:23 +0200

    Add files via upload

---
+++ RETO_EXPERTO_390_400_410_420/RETO_EXPERTO_390_400_410_420_PREGUNTA2/RETO_EXPERTO_390_400_410_420_PREGUNTA_2_ALEAT_57/RETO_EXPERTO_390_400_410_420_PREGUNTA_2_ALEAT_57.py
@@ -0,0 +1,1 @@
+['0.1191', '12.9216', '0.1142', "Los valores de la variable 'eosinofilos' siguen una distribución Normal", "Los valores de la variable 'eosinofilos' no siguen una distribución Normal"]
