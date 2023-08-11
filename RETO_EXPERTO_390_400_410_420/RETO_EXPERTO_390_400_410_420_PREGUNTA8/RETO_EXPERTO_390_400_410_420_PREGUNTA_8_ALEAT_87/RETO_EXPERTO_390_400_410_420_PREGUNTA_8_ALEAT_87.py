52f8e6fee889893cba73f5e0ca2ced5b99cfe631
pepemm <106663746+pepemm@users.noreply.github.com>
2023-08-11 17:29:08 +0200

    Add files via upload

---
+++ RETO_EXPERTO_390_400_410_420/RETO_EXPERTO_390_400_410_420_PREGUNTA8/RETO_EXPERTO_390_400_410_420_PREGUNTA_8_ALEAT_87/RETO_EXPERTO_390_400_410_420_PREGUNTA_8_ALEAT_87.py
@@ -0,0 +1,1 @@
+['5.1336', "Las medias poblacionales de 'urea' de los grupos de la variable 'target' son significativamente iguales", '0.0075', "Al menos, la media poblacional de 'urea' de uno de los grupos de la variable 'target' es significativamente diferente a la de otro grupo", '0.2902']
