fd6332a5147fa30069d7368670bbe8b268e16c4b
pepemm <106663746+pepemm@users.noreply.github.com>
2023-08-11 12:05:10 +0200

    Add files via upload

---
+++ RETO_EXPERTO_390_400_410_420/RETO_EXPERTO_390_400_410_420_PREGUNTA6/RETO_EXPERTO_390_400_410_420_PREGUNTA_6_ALEAT_85/RETO_EXPERTO_390_400_410_420_PREGUNTA_6_ALEAT_85.py
@@ -0,0 +1,1 @@
+['0.4432', '0.574', "La media poblacional de 'neutrofilos' del grupo 'mujer' es significativamente igual a la del grupo 'hombre'", "La media poblacional de 'neutrofilos' del grupo 'mujer' es significativamente diferente a la del grupo 'hombre'", '0.7717']
