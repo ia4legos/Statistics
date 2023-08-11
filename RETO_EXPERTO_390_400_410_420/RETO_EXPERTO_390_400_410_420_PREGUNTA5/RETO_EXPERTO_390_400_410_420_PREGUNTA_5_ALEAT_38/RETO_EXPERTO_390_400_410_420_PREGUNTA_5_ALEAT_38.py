50d8ee0679860aaf4426d1d47d5e9c29a4bb1ab7
pepemm <106663746+pepemm@users.noreply.github.com>
2023-08-11 11:46:19 +0200

    Add files via upload

---
+++ RETO_EXPERTO_390_400_410_420/RETO_EXPERTO_390_400_410_420_PREGUNTA5/RETO_EXPERTO_390_400_410_420_PREGUNTA_5_ALEAT_38/RETO_EXPERTO_390_400_410_420_PREGUNTA_5_ALEAT_38.py
@@ -0,0 +1,1 @@
+['0.1951', "La media poblacional de 'monocitos' del grupo 'mujer' es significativamente igual a la del grupo 'hombre'", "La media poblacional de 'monocitos' del grupo 'mujer' es significativamente diferente a la del grupo 'hombre'", '0.8299', '1.3043']
