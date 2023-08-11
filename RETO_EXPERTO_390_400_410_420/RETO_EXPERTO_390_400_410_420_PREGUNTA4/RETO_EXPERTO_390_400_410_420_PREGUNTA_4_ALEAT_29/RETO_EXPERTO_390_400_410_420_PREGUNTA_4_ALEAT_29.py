789c7cdbeb3eec1cbb6139226b95d9dda0b86afa
pepemm <106663746+pepemm@users.noreply.github.com>
2023-08-11 11:27:28 +0200

    Add files via upload

---
+++ RETO_EXPERTO_390_400_410_420/RETO_EXPERTO_390_400_410_420_PREGUNTA4/RETO_EXPERTO_390_400_410_420_PREGUNTA_4_ALEAT_29/RETO_EXPERTO_390_400_410_420_PREGUNTA_4_ALEAT_29.py
@@ -0,0 +1,1 @@
+['0.6952', "La varianza poblacional de 'leucocitos' del grupo 'mujer' es significativamente diferente a la del grupo 'hombre'", "La varianza poblacional de 'leucocitos' del grupo 'mujer' es significativamente igual a la del grupo 'hombre'", '0.2386', '0.996']
