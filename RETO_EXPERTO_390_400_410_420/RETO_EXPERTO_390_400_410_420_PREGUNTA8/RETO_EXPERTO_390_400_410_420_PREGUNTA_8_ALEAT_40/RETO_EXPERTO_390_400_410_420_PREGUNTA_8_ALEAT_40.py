36868398991fc88eb29e4072dab9180e9afd7389
pepemm <106663746+pepemm@users.noreply.github.com>
2023-08-11 13:31:15 +0200

    Add files via upload

---
+++ RETO_EXPERTO_390_400_410_420/RETO_EXPERTO_390_400_410_420_PREGUNTA8/RETO_EXPERTO_390_400_410_420_PREGUNTA_8_ALEAT_40/RETO_EXPERTO_390_400_410_420_PREGUNTA_8_ALEAT_40.py
@@ -0,0 +1,1 @@
+["Al menos, la media poblacional de 'edad' de uno de los grupos de la variable 'target' es significativamente diferente a la de otro grupo", "Las medias poblacionales de 'edad' de los grupos de la variable 'target' son significativamente iguales", '0.048', '3.171', '0.7756']
