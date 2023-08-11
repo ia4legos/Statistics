88d9300ca92979d6b54b0df5c84dd6692ecc2b5c
pepemm <106663746+pepemm@users.noreply.github.com>
2023-08-11 11:44:15 +0200

    Add files via upload

---
+++ RETO_EXPERTO_390_400_410_420/RETO_EXPERTO_390_400_410_420_PREGUNTA5/RETO_EXPERTO_390_400_410_420_PREGUNTA_5_ALEAT_3/RETO_EXPERTO_390_400_410_420_PREGUNTA_5_ALEAT_3.py
@@ -0,0 +1,1 @@
+["La media poblacional de 'glucosa' del grupo 'mujer' no es significativamente menor que la del grupo 'hombre'", "La media poblacional de 'glucosa' del grupo 'mujer' es significativamente menor que la del grupo 'hombre'", '0.1885', '19.6229', '0.5744']
