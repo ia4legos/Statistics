0a5cd908854cd083a62262c58836d54f467eda10
pepemm <106663746+pepemm@users.noreply.github.com>
2023-08-11 13:34:48 +0200

    Add files via upload

---
+++ RETO_EXPERTO_390_400_410_420/RETO_EXPERTO_390_400_410_420_PREGUNTA8/RETO_EXPERTO_390_400_410_420_PREGUNTA_8_ALEAT_91/RETO_EXPERTO_390_400_410_420_PREGUNTA_8_ALEAT_91.py
@@ -0,0 +1,1 @@
+['0.926', '0.077', "Las medias poblacionales de 'pcr' de los grupos de la variable 'target' son significativamente iguales", "Al menos, la media poblacional de 'pcr' de uno de los grupos de la variable 'target' es significativamente diferente a la de otro grupo", '0.8722']
