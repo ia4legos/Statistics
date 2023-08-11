58ff222309a8e180fb9fac0f922b945a85fe9637
pepemm <106663746+pepemm@users.noreply.github.com>
2023-08-11 17:24:49 +0200

    Add files via upload

---
+++ RETO_EXPERTO_390_400_410_420/RETO_EXPERTO_390_400_410_420_PREGUNTA8/RETO_EXPERTO_390_400_410_420_PREGUNTA_8_ALEAT_13/RETO_EXPERTO_390_400_410_420_PREGUNTA_8_ALEAT_13.py
@@ -0,0 +1,1 @@
+['0.7342', "Las medias poblacionales de 'monocitos' de los grupos de la variable 'target' son significativamente iguales", '1.5491', '0.2175', "Al menos, la media poblacional de 'monocitos' de uno de los grupos de la variable 'target' es significativamente diferente a la de otro grupo"]
