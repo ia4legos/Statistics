82e0bf48add9a4836760bd2ccf328a2d2c3b84fd
pepemm <106663746+pepemm@users.noreply.github.com>
2023-08-11 11:26:30 +0200

    Add files via upload

---
+++ RETO_EXPERTO_390_400_410_420/RETO_EXPERTO_390_400_410_420_PREGUNTA4/RETO_EXPERTO_390_400_410_420_PREGUNTA_4_ALEAT_13/RETO_EXPERTO_390_400_410_420_PREGUNTA_4_ALEAT_13.py
@@ -0,0 +1,1 @@
+["La varianza poblacional de 'linfocitos' del grupo 'mujer' es significativamente diferente a la del grupo 'hombre'", '0.1885', '0.8884', '1.0436', "La varianza poblacional de 'linfocitos' del grupo 'mujer' es significativamente igual a la del grupo 'hombre'"]
