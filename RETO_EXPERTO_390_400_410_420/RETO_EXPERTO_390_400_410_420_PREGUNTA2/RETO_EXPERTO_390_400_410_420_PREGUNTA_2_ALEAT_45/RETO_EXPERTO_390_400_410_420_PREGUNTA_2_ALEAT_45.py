18f40030ecd52b0cdc47535faa8c37b6c0adb9f8
pepemm <106663746+pepemm@users.noreply.github.com>
2023-08-11 10:51:34 +0200

    Add files via upload

---
+++ RETO_EXPERTO_390_400_410_420/RETO_EXPERTO_390_400_410_420_PREGUNTA2/RETO_EXPERTO_390_400_410_420_PREGUNTA_2_ALEAT_45/RETO_EXPERTO_390_400_410_420_PREGUNTA_2_ALEAT_45.py
@@ -0,0 +1,1 @@
+["Los valores de la variable 'neutrofilos' siguen una distribución Normal", "Los valores de la variable 'neutrofilos' no siguen una distribución Normal", '0.3376', '0.1189', '0.8224']
