ea0bf80d428368435eb193153a6bb02cf16f2816
pepemm <106663746+pepemm@users.noreply.github.com>
2023-08-11 17:28:17 +0200

    Add files via upload

---
+++ RETO_EXPERTO_390_400_410_420/RETO_EXPERTO_390_400_410_420_PREGUNTA8/RETO_EXPERTO_390_400_410_420_PREGUNTA_8_ALEAT_72/RETO_EXPERTO_390_400_410_420_PREGUNTA_8_ALEAT_72.py
@@ -0,0 +1,1 @@
+['0.2112', "Las medias poblacionales de 'basofilos' de los grupos de la variable 'target' son significativamente iguales", "Al menos, la media poblacional de 'basofilos' de uno de los grupos de la variable 'target' es significativamente diferente a la de otro grupo", '1.5804', '0.2959']
