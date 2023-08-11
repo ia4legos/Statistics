25768d7b020bc9db4989a63c092c22a4c9cab3c1
pepemm <106663746+pepemm@users.noreply.github.com>
2023-08-11 13:32:39 +0200

    Add files via upload

---
+++ RETO_EXPERTO_390_400_410_420/RETO_EXPERTO_390_400_410_420_PREGUNTA8/RETO_EXPERTO_390_400_410_420_PREGUNTA_8_ALEAT_60/RETO_EXPERTO_390_400_410_420_PREGUNTA_8_ALEAT_60.py
@@ -0,0 +1,1 @@
+['0.0001', '10.1611', '0.9347', "Las medias poblacionales de 'leucocitos' de los grupos de la variable 'target' son significativamente iguales", "Al menos, la media poblacional de 'leucocitos' de uno de los grupos de la variable 'target' es significativamente diferente a la de otro grupo"]
