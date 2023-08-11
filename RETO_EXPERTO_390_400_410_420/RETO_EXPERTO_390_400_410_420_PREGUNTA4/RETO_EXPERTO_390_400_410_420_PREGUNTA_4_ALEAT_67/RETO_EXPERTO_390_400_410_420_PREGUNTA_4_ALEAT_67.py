12368719b62335609df19287d4f483f552ece1b5
pepemm <106663746+pepemm@users.noreply.github.com>
2023-08-11 11:29:46 +0200

    Add files via upload

---
+++ RETO_EXPERTO_390_400_410_420/RETO_EXPERTO_390_400_410_420_PREGUNTA4/RETO_EXPERTO_390_400_410_420_PREGUNTA_4_ALEAT_67/RETO_EXPERTO_390_400_410_420_PREGUNTA_4_ALEAT_67.py
@@ -0,0 +1,1 @@
+['0.9018', '0.7294', "La varianza poblacional de 'sodio' del grupo 'mujer' es significativamente igual a la del grupo 'hombre'", '0.2698', "La varianza poblacional de 'sodio' del grupo 'mujer' es significativamente diferente a la del grupo 'hombre'"]
