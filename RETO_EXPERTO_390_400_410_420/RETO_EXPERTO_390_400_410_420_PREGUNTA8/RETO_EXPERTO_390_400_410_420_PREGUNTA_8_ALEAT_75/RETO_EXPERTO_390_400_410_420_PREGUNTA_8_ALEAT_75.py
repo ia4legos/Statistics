69a1862f716aaca4185abef9fa6053fe657e9f79
pepemm <106663746+pepemm@users.noreply.github.com>
2023-08-11 13:33:41 +0200

    Add files via upload

---
+++ RETO_EXPERTO_390_400_410_420/RETO_EXPERTO_390_400_410_420_PREGUNTA8/RETO_EXPERTO_390_400_410_420_PREGUNTA_8_ALEAT_75/RETO_EXPERTO_390_400_410_420_PREGUNTA_8_ALEAT_75.py
@@ -0,0 +1,1 @@
+['0.6883', '0.0195', "Al menos, la media poblacional de 'eosinofilos' de uno de los grupos de la variable 'target' es significativamente diferente a la de otro grupo", '4.0903', "Las medias poblacionales de 'eosinofilos' de los grupos de la variable 'target' son significativamente iguales"]
