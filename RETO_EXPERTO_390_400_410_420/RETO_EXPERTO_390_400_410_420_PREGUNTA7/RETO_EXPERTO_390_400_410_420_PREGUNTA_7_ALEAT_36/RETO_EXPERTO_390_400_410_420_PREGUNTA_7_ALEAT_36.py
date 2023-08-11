8c1c93d3c6e296a8a2204159d243602b3ccd431e
pepemm <106663746+pepemm@users.noreply.github.com>
2023-08-11 13:12:57 +0200

    Add files via upload

---
+++ RETO_EXPERTO_390_400_410_420/RETO_EXPERTO_390_400_410_420_PREGUNTA7/RETO_EXPERTO_390_400_410_420_PREGUNTA_7_ALEAT_36/RETO_EXPERTO_390_400_410_420_PREGUNTA_7_ALEAT_36.py
@@ -0,0 +1,1 @@
+['Suma de Cuadrados Dentro = 0.020048894858829193', 'Suma de Cuadrados Dentro = 0.040097789717658386', 'Suma de Cuadrados Dentro = 16.286203095238097', 'Suma de Cuadrados Dentro = 2.0', 'Suma de Cuadrados Dentro = 110.0']
