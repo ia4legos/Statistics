fd712c8e2e3c6b37ebc3daf59ae511d20645a3e6
pepemm <106663746+pepemm@users.noreply.github.com>
2023-08-11 11:29:24 +0200

    Add files via upload

---
+++ RETO_EXPERTO_390_400_410_420/RETO_EXPERTO_390_400_410_420_PREGUNTA4/RETO_EXPERTO_390_400_410_420_PREGUNTA_4_ALEAT_61/RETO_EXPERTO_390_400_410_420_PREGUNTA_4_ALEAT_61.py
@@ -0,0 +1,1 @@
+['0.0148', '0.4811', "La varianza poblacional de 'pcr' del grupo 'mujer' es significativamente igual a la del grupo 'hombre'", '0.4585', "La varianza poblacional de 'pcr' del grupo 'mujer' es significativamente diferente a la del grupo 'hombre'"]
