bac43d91f2dce553d391a8d0188caee2c3f0d73c
pepemm <106663746+pepemm@users.noreply.github.com>
2023-08-11 11:31:13 +0200

    Add files via upload

---
+++ RETO_EXPERTO_390_400_410_420/RETO_EXPERTO_390_400_410_420_PREGUNTA4/RETO_EXPERTO_390_400_410_420_PREGUNTA_4_ALEAT_91/RETO_EXPERTO_390_400_410_420_PREGUNTA_4_ALEAT_91.py
@@ -0,0 +1,1 @@
+['0.6476', '0.9368', "La varianza poblacional de 'neutrofilos' del grupo 'hombre' es significativamente igual a la del grupo 'mujer'", "La varianza poblacional de 'neutrofilos' del grupo 'hombre' es significativamente diferente a la del grupo 'mujer'", '0.176']
