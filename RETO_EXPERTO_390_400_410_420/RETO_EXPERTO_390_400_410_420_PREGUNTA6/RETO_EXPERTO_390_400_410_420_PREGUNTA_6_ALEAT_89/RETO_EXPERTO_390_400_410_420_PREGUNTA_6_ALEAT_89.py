de3cb952b1b1e915d6f387bd9da5fc0f2a5ee662
pepemm <106663746+pepemm@users.noreply.github.com>
2023-08-11 12:05:25 +0200

    Add files via upload

---
+++ RETO_EXPERTO_390_400_410_420/RETO_EXPERTO_390_400_410_420_PREGUNTA6/RETO_EXPERTO_390_400_410_420_PREGUNTA_6_ALEAT_89/RETO_EXPERTO_390_400_410_420_PREGUNTA_6_ALEAT_89.py
@@ -0,0 +1,1 @@
+['0.9537', "La media poblacional de 'basofilos' del grupo 'mujer' no es significativamente mayor que la del grupo 'hombre'", "La media poblacional de 'basofilos' del grupo 'mujer' es significativamente mayor que la del grupo 'hombre'", '-1.7033', '0.1332']
