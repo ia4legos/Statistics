a35df2a32e765e921bd0466c61c1f9990f7d5a21
pepemm <106663746+pepemm@users.noreply.github.com>
2023-08-11 11:28:52 +0200

    Add files via upload

---
+++ RETO_EXPERTO_390_400_410_420/RETO_EXPERTO_390_400_410_420_PREGUNTA4/RETO_EXPERTO_390_400_410_420_PREGUNTA_4_ALEAT_52/RETO_EXPERTO_390_400_410_420_PREGUNTA_4_ALEAT_52.py
@@ -0,0 +1,1 @@
+["La varianza poblacional de 'glucosa' del grupo 'hombre' es significativamente diferente a la del grupo 'mujer'", '0.3838', '0.8599', "La varianza poblacional de 'glucosa' del grupo 'hombre' es significativamente igual a la del grupo 'mujer'", '0.6073']
