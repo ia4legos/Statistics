78a17f76821d6e118be279f329207c7e3bf7da74
pepemm <106663746+pepemm@users.noreply.github.com>
2023-08-11 12:01:22 +0200

    Add files via upload

---
+++ RETO_EXPERTO_390_400_410_420/RETO_EXPERTO_390_400_410_420_PREGUNTA6/RETO_EXPERTO_390_400_410_420_PREGUNTA_6_ALEAT_22/RETO_EXPERTO_390_400_410_420_PREGUNTA_6_ALEAT_22.py
@@ -0,0 +1,1 @@
+["La media poblacional de 'leucocitos' del grupo 'mujer' es significativamente igual a la del grupo 'hombre'", "La media poblacional de 'leucocitos' del grupo 'mujer' es significativamente diferente a la del grupo 'hombre'", '11.4872', '0.8328', '0.4073']
