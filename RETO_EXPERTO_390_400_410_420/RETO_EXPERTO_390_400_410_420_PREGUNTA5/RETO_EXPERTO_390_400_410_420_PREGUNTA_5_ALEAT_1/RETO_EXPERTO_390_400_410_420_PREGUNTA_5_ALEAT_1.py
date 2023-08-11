7b4b9e19825ee71dd78a150b29f6f8f5e77eab85
pepemm <106663746+pepemm@users.noreply.github.com>
2023-08-11 11:44:08 +0200

    Add files via upload

---
+++ RETO_EXPERTO_390_400_410_420/RETO_EXPERTO_390_400_410_420_PREGUNTA5/RETO_EXPERTO_390_400_410_420_PREGUNTA_5_ALEAT_1/RETO_EXPERTO_390_400_410_420_PREGUNTA_5_ALEAT_1.py
@@ -0,0 +1,1 @@
+['0.5878', '0.0259', "La media poblacional de 'inl' del grupo 'mujer' es significativamente mayor que la del grupo 'hombre'", "La media poblacional de 'inl' del grupo 'mujer' no es significativamente mayor que la del grupo 'hombre'", '1.9655']
