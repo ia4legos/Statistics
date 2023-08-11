5d5184103df5202fbd5cfd8c4ff3664d43812dcf
pepemm <106663746+pepemm@users.noreply.github.com>
2023-08-11 11:30:03 +0200

    Add files via upload

---
+++ RETO_EXPERTO_390_400_410_420/RETO_EXPERTO_390_400_410_420_PREGUNTA4/RETO_EXPERTO_390_400_410_420_PREGUNTA_4_ALEAT_72/RETO_EXPERTO_390_400_410_420_PREGUNTA_4_ALEAT_72.py
@@ -0,0 +1,1 @@
+["La varianza poblacional de 'basofilos' del grupo 'hombre' es significativamente igual a la del grupo 'mujer'", '0.3662', "La varianza poblacional de 'basofilos' del grupo 'hombre' es significativamente diferente a la del grupo 'mujer'", '0.6368', '1.1554']
