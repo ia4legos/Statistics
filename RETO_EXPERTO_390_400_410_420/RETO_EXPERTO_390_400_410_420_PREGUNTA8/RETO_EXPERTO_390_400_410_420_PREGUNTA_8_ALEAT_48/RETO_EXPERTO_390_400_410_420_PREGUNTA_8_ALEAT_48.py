6df0f3163eb8c74306935b310558a99476e1701f
pepemm <106663746+pepemm@users.noreply.github.com>
2023-08-11 17:26:53 +0200

    Add files via upload

---
+++ RETO_EXPERTO_390_400_410_420/RETO_EXPERTO_390_400_410_420_PREGUNTA8/RETO_EXPERTO_390_400_410_420_PREGUNTA_8_ALEAT_48/RETO_EXPERTO_390_400_410_420_PREGUNTA_8_ALEAT_48.py
@@ -0,0 +1,1 @@
+['0.067', '0.1628', "Al menos, la media poblacional de 'glucosa' de uno de los grupos de la variable 'target' es significativamente diferente a la de otro grupo", '1.8468', "Las medias poblacionales de 'glucosa' de los grupos de la variable 'target' son significativamente iguales"]
