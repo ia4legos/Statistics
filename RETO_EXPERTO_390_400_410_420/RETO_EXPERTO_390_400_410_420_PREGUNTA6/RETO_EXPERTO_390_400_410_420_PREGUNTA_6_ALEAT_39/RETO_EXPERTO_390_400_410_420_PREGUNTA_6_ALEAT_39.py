2f6e1291ffd4310a70e1d8a2e30a0a2efd883f1d
pepemm <106663746+pepemm@users.noreply.github.com>
2023-08-11 12:02:24 +0200

    Add files via upload

---
+++ RETO_EXPERTO_390_400_410_420/RETO_EXPERTO_390_400_410_420_PREGUNTA6/RETO_EXPERTO_390_400_410_420_PREGUNTA_6_ALEAT_39/RETO_EXPERTO_390_400_410_420_PREGUNTA_6_ALEAT_39.py
@@ -0,0 +1,1 @@
+['0.5778', "La media poblacional de 'edad' del grupo 'mujer' es significativamente diferente a la del grupo 'hombre'", '0.1157', '0.5657', "La media poblacional de 'edad' del grupo 'mujer' es significativamente igual a la del grupo 'hombre'"]
