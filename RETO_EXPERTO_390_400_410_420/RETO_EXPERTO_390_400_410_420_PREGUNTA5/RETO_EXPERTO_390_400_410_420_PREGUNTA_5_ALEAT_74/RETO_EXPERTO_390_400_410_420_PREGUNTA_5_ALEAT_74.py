eabee05d3bc9a9403bdab9fd3c3156694b1762c6
pepemm <106663746+pepemm@users.noreply.github.com>
2023-08-11 11:48:27 +0200

    Add files via upload

---
+++ RETO_EXPERTO_390_400_410_420/RETO_EXPERTO_390_400_410_420_PREGUNTA5/RETO_EXPERTO_390_400_410_420_PREGUNTA_5_ALEAT_74/RETO_EXPERTO_390_400_410_420_PREGUNTA_5_ALEAT_74.py
@@ -0,0 +1,1 @@
+["La media poblacional de 'linfocitos' del grupo 'mujer' es significativamente mayor que la del grupo 'hombre'", "La media poblacional de 'linfocitos' del grupo 'mujer' no es significativamente mayor que la del grupo 'hombre'", '0.0991', '1.3048', '0.1549']
