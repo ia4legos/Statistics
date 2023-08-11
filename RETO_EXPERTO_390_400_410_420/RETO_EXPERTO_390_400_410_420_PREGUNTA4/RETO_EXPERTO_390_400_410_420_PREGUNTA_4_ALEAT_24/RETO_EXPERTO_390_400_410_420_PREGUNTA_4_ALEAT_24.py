77599cce9b3fe37b827dd2416f44b77751fcea51
pepemm <106663746+pepemm@users.noreply.github.com>
2023-08-11 11:27:10 +0200

    Add files via upload

---
+++ RETO_EXPERTO_390_400_410_420/RETO_EXPERTO_390_400_410_420_PREGUNTA4/RETO_EXPERTO_390_400_410_420_PREGUNTA_4_ALEAT_24/RETO_EXPERTO_390_400_410_420_PREGUNTA_4_ALEAT_24.py
@@ -0,0 +1,1 @@
+['0.8579', "La varianza poblacional de 'edad' del grupo 'hombre' es significativamente igual a la del grupo 'mujer'", '0.9544', "La varianza poblacional de 'edad' del grupo 'hombre' es significativamente diferente a la del grupo 'mujer'", '0.4392']
