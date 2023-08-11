77a6ac05b44dbbe631a91cdda56c2a593701bf81
pepemm <106663746+pepemm@users.noreply.github.com>
2023-08-11 17:29:49 +0200

    Add files via upload

---
+++ RETO_EXPERTO_390_400_410_420/RETO_EXPERTO_390_400_410_420_PREGUNTA8/RETO_EXPERTO_390_400_410_420_PREGUNTA_8_ALEAT_99/RETO_EXPERTO_390_400_410_420_PREGUNTA_8_ALEAT_99.py
@@ -0,0 +1,1 @@
+['2.8518', "Al menos, la media poblacional de 'sodio' de uno de los grupos de la variable 'target' es significativamente diferente a la de otro grupo", '0.0619', "Las medias poblacionales de 'sodio' de los grupos de la variable 'target' son significativamente iguales", '0.1665']
