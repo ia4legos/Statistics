b291bf6dd438a8ac97b21b0b919f26c7357986a1
pepemm <106663746+pepemm@users.noreply.github.com>
2023-08-11 13:34:11 +0200

    Add files via upload

---
+++ RETO_EXPERTO_390_400_410_420/RETO_EXPERTO_390_400_410_420_PREGUNTA8/RETO_EXPERTO_390_400_410_420_PREGUNTA_8_ALEAT_82/RETO_EXPERTO_390_400_410_420_PREGUNTA_8_ALEAT_82.py
@@ -0,0 +1,1 @@
+['0.7978', "Las medias poblacionales de 'neutrofilos' de los grupos de la variable 'target' son significativamente iguales", "Al menos, la media poblacional de 'neutrofilos' de uno de los grupos de la variable 'target' es significativamente diferente a la de otro grupo", '8.3398', '0.0006']
