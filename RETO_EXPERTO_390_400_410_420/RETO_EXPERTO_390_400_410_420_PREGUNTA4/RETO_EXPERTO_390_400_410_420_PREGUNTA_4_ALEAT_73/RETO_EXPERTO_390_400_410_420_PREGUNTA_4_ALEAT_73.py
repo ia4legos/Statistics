daa6bdbe15e5cfee88221c185119572a362b3fb4
pepemm <106663746+pepemm@users.noreply.github.com>
2023-08-11 11:30:07 +0200

    Add files via upload

---
+++ RETO_EXPERTO_390_400_410_420/RETO_EXPERTO_390_400_410_420_PREGUNTA4/RETO_EXPERTO_390_400_410_420_PREGUNTA_4_ALEAT_73/RETO_EXPERTO_390_400_410_420_PREGUNTA_4_ALEAT_73.py
@@ -0,0 +1,1 @@
+['0.1449', "La varianza poblacional de 'urea' del grupo 'mujer' es significativamente diferente a la del grupo 'hombre'", '0.0647', '0.498', "La varianza poblacional de 'urea' del grupo 'mujer' es significativamente igual a la del grupo 'hombre'"]
