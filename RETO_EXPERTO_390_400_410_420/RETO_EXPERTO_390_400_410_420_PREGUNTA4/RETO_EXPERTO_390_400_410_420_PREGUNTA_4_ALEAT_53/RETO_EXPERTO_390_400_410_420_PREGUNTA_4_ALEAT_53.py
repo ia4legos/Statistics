a2ed36e814b7db8b4c1da7d120a9a20e9be05cc3
pepemm <106663746+pepemm@users.noreply.github.com>
2023-08-11 11:28:55 +0200

    Add files via upload

---
+++ RETO_EXPERTO_390_400_410_420/RETO_EXPERTO_390_400_410_420_PREGUNTA4/RETO_EXPERTO_390_400_410_420_PREGUNTA_4_ALEAT_53/RETO_EXPERTO_390_400_410_420_PREGUNTA_4_ALEAT_53.py
@@ -0,0 +1,1 @@
+["La varianza poblacional de 'monocitos' del grupo 'hombre' es significativamente diferente a la del grupo 'mujer'", '0.627', '0.9003', "La varianza poblacional de 'monocitos' del grupo 'hombre' es significativamente igual a la del grupo 'mujer'", '0.9643']
