bbbfc1fdbcd0a1795ced69a7bf8e37e02aff401a
pepemm <106663746+pepemm@users.noreply.github.com>
2023-08-11 10:48:53 +0200

    Add files via upload

---
+++ RETO_EXPERTO_390_400_410_420/RETO_EXPERTO_390_400_410_420_PREGUNTA2/RETO_EXPERTO_390_400_410_420_PREGUNTA_2_ALEAT_8/RETO_EXPERTO_390_400_410_420_PREGUNTA_2_ALEAT_8.py
@@ -0,0 +1,1 @@
+["Los valores de la variable 'leucocitos' no siguen una distribución Normal", "Los valores de la variable 'leucocitos' siguen una distribución Normal", '25.4149', '0.0683', '0.8248']
