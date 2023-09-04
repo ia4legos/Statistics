f7f593e1bd6b4e74c895250509e068382a4bb805
pepemm <106663746+pepemm@users.noreply.github.com>
2023-09-04 14:57:24 +0200

    Add files via upload

---
+++ RETO_EXPERTO_390_400_410_420/RETO_EXPERTO_390_400_410_420_PREGUNTA10/RETO_EXPERTO_390_400_410_420_PREGUNTA_10_ALEAT_41/RETO_EXPERTO_390_400_410_420_PREGUNTA_10_ALEAT_41.py
@@ -0,0 +1,1 @@
+["La media poblacional de 'sodio' del grupo 'mujer' no es significativamente mayor que la del grupo 'hombre'", '0.4158732411804413', '556.5', '471.115412096709', "La media poblacional de 'sodio' del grupo 'mujer' es significativamente mayor que la del grupo 'hombre'"]
