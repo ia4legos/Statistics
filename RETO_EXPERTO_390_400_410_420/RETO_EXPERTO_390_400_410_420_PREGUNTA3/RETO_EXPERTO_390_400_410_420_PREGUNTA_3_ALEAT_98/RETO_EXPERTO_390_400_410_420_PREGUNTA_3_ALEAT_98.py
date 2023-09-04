c5bf6916ddd8033ad67bd8e76e07fdfc3ca496f6
pepemm <106663746+pepemm@users.noreply.github.com>
2023-09-04 12:57:39 +0200

    Add files via upload

---
+++ RETO_EXPERTO_390_400_410_420/RETO_EXPERTO_390_400_410_420_PREGUNTA3/RETO_EXPERTO_390_400_410_420_PREGUNTA_3_ALEAT_98/RETO_EXPERTO_390_400_410_420_PREGUNTA_3_ALEAT_98.py
@@ -0,0 +1,1 @@
+['La media poblacional de eosinofilos no es significativamente mayor que 0.42', '35.1345', '6.8382', '0.0', 'La media poblacional de eosinofilos es significativamente mayor que 0.42']
