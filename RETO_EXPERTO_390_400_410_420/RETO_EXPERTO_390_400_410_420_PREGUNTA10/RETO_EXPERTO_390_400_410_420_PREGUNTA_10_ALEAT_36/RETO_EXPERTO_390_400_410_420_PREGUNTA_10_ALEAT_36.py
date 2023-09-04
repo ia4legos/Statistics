4d3f1045ecb268a4634b2d4e51ccb7d09d37bbc7
pepemm <106663746+pepemm@users.noreply.github.com>
2023-09-04 14:57:03 +0200

    Add files via upload

---
+++ RETO_EXPERTO_390_400_410_420/RETO_EXPERTO_390_400_410_420_PREGUNTA10/RETO_EXPERTO_390_400_410_420_PREGUNTA_10_ALEAT_36/RETO_EXPERTO_390_400_410_420_PREGUNTA_10_ALEAT_36.py
@@ -0,0 +1,1 @@
+["La media poblacional de 'urea' del grupo 'mujer' no es significativamente menor que la del grupo 'hombre'", '0.6641767720013184', "La media poblacional de 'urea' del grupo 'mujer' es significativamente menor que la del grupo 'hombre'", '0.5280817612919715', '355.5']
