ef875b902d92e88069952667907080cede7898b3
pepemm <106663746+pepemm@users.noreply.github.com>
2023-09-04 14:57:07 +0200

    Add files via upload

---
+++ RETO_EXPERTO_390_400_410_420/RETO_EXPERTO_390_400_410_420_PREGUNTA10/RETO_EXPERTO_390_400_410_420_PREGUNTA_10_ALEAT_37/RETO_EXPERTO_390_400_410_420_PREGUNTA_10_ALEAT_37.py
@@ -0,0 +1,1 @@
+['0.6085324642931085', '0.7924021580517995', "La media poblacional de 'potasio' del grupo 'mujer' no es significativamente mayor que la del grupo 'hombre'", "La media poblacional de 'potasio' del grupo 'mujer' es significativamente mayor que la del grupo 'hombre'", '291.0']
