973a8c6bd79f0b9afe49ac97524648214fdb240b
pepemm <106663746+pepemm@users.noreply.github.com>
2023-09-04 14:59:37 +0200

    Add files via upload

---
+++ RETO_EXPERTO_390_400_410_420/RETO_EXPERTO_390_400_410_420_PREGUNTA10/RETO_EXPERTO_390_400_410_420_PREGUNTA_10_ALEAT_71/RETO_EXPERTO_390_400_410_420_PREGUNTA_10_ALEAT_71.py
@@ -0,0 +1,1 @@
+["Al menos, la media poblacional de 'linfocitos' de uno de los grupos de la variable 'target' es significativamente diferente a la de otro grupo", '0.5858803877925981', '9.384189468480484', "Las medias poblacionales de 'linfocitos' de los grupos de la variable 'target' son significativamente iguales", '0.009167462631316027']
