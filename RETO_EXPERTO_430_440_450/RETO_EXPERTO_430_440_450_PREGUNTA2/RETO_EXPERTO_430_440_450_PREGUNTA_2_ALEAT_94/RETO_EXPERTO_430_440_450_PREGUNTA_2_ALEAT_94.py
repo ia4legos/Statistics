0765e45a83f4d646b5468943be2b38ad4e351251
pepemm <106663746+pepemm@users.noreply.github.com>
2023-09-06 08:43:06 +0200

    Add files via upload

---
+++ RETO_EXPERTO_430_440_450/RETO_EXPERTO_430_440_450_PREGUNTA2/RETO_EXPERTO_430_440_450_PREGUNTA_2_ALEAT_94/RETO_EXPERTO_430_440_450_PREGUNTA_2_ALEAT_94.py
@@ -0,0 +1,1 @@
+['linfocitos = 72.2472 - 0.7413*edad', 'linfocitos = 72.2472 + 0.7413*neutrofilos', 'linfocitos = 72.2472 - 0.7413*neutrofilos', 'linfocitos = -0.7413 - 72.2472*neutrofilos', 'neutrofilos = 72.2472 - 0.7413*linfocitos']
