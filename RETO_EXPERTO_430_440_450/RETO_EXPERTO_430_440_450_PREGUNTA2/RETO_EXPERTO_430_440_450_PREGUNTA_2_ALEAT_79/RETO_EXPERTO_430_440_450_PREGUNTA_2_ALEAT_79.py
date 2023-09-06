cdcab05b2acdb80967ef24a469d9601aa851bdba
pepemm <106663746+pepemm@users.noreply.github.com>
2023-09-06 08:41:39 +0200

    Add files via upload

---
+++ RETO_EXPERTO_430_440_450/RETO_EXPERTO_430_440_450_PREGUNTA2/RETO_EXPERTO_430_440_450_PREGUNTA_2_ALEAT_79/RETO_EXPERTO_430_440_450_PREGUNTA_2_ALEAT_79.py
@@ -0,0 +1,1 @@
+['basofilos = 1.0128 + 0.1265*potasio', 'basofilos = 1.0128 - 0.1265*potasio', 'potasio = 1.0128 - 0.1265*basofilos', 'basofilos = -0.1265 - 1.0128*potasio', 'basofilos = 1.0128 - 0.1265*neutrofilos']
