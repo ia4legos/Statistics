24801f17f1d287197cb7904380c7be21499570da
pepemm <106663746+pepemm@users.noreply.github.com>
2023-09-06 16:37:07 +0200

    Add files via upload

---
+++ RETO_EXPERTO_430_440_450/RETO_EXPERTO_430_440_450_PREGUNTA8/RETO_EXPERTO_430_440_450_PREGUNTA_8_ALEAT_65/RETO_EXPERTO_430_440_450_PREGUNTA_8_ALEAT_65.py
@@ -0,0 +1,1 @@
+['-2.7474', "El parámetro de la variable 'eosinofilos' es significativamente diferente de 0", "El parámetro de la variable 'urea' es significativamente igual a 0", '0.0071', "El parámetro de la variable 'urea' es significativamente diferente de 0"]
