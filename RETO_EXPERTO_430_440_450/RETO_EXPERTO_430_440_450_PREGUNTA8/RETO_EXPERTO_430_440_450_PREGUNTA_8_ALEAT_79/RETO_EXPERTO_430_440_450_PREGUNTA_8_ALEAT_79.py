02a3d1fb8384ad2fe91954b9c471abb6c4c55fc4
pepemm <106663746+pepemm@users.noreply.github.com>
2023-09-06 16:37:56 +0200

    Add files via upload

---
+++ RETO_EXPERTO_430_440_450/RETO_EXPERTO_430_440_450_PREGUNTA8/RETO_EXPERTO_430_440_450_PREGUNTA_8_ALEAT_79/RETO_EXPERTO_430_440_450_PREGUNTA_8_ALEAT_79.py
@@ -0,0 +1,1 @@
+['0.1488', "El parámetro de la variable 'leucocitos' es significativamente igual a 0", '-2.8273', '0.882', "El parámetro de la variable 'leucocitos' es significativamente diferente de 0"]
