a471dcd8f9cd254a2e238cea0a8d62a9d61e9793
pepemm <106663746+pepemm@users.noreply.github.com>
2023-09-06 16:38:44 +0200

    Add files via upload

---
+++ RETO_EXPERTO_430_440_450/RETO_EXPERTO_430_440_450_PREGUNTA8/RETO_EXPERTO_430_440_450_PREGUNTA_8_ALEAT_92/RETO_EXPERTO_430_440_450_PREGUNTA_8_ALEAT_92.py
@@ -0,0 +1,1 @@
+['-0.614', '0.0', '-21.2129', "El parámetro de la variable 'neutrofilos' es significativamente igual a 0", "El parámetro de la variable 'neutrofilos' es significativamente diferente de 0"]
